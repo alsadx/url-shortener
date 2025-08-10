230b9a6fb984434c6c5bf3449bdb41c8bac5536d
Raccoon Slow <raccoons@student.21-school.ru>
2025-08-10 11:21:24 +0300

    feat: add functional tests

---
+++ tests/url_shortener_test.go
@@ -0,0 +1,141 @@
+package tests
+
+import (
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/url"
+	"path"
+	"testing"
+
+	"url-shortener/internal/http-server/handlers/url/save"
+	"url-shortener/internal/lib/api"
+	"url-shortener/internal/lib/random"
+)
+
+const host = "localhost:8082"
+
+func TestURLShortener_HappyPath(t *testing.T) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+	}
+	e := httpexpect.Default(t, u.String())
+
+	str, _ := random.NewRandomString(10)
+
+	e.POST("/url").
+		WithJSON(save.Request{
+			URL:   gofakeit.URL(),
+			Alias: str,
+		}).
+		WithBasicAuth("admin", "admin").
+		Expect().
+		Status(200).
+		JSON().
+		Object().
+		ContainsKey("alias")
+}
+
+//nolint:funlen
+func TestURLShortener_SaveRedirectDelete(t *testing.T) {
+	testCases := []struct {
+		name  string
+		url   string
+		alias string
+		error string
+	}{
+		{
+			name:  "Valid URL",
+			url:   gofakeit.URL(),
+			alias: gofakeit.Word() + gofakeit.Word(),
+		},
+		{
+			name:  "Invalid URL",
+			url:   "invalid_url",
+			alias: gofakeit.Word(),
+			error: "URL is not a valid URL",
+		},
+		{
+			name:  "Empty Alias",
+			url:   gofakeit.URL(),
+			alias: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			u := url.URL{
+				Scheme: "http",
+				Host:   host,
+			}
+
+			e := httpexpect.Default(t, u.String())
+
+			// Save
+			req := e.POST("/url").
+				WithJSON(save.Request{
+					URL:   tc.url,
+					Alias: tc.alias,
+				}).
+				WithBasicAuth("admin", "admin").
+				Expect()
+
+			if tc.error != "" {
+				req.Status(http.StatusBadRequest)
+				req.Body().Contains(tc.error)
+				return
+			}
+
+			req.Status(http.StatusOK)
+			resp := req.JSON().Object()
+
+			alias := tc.alias
+
+			if tc.alias != "" {
+				resp.Value("alias").String().IsEqual(tc.alias)
+			} else {
+				resp.Value("alias").String().NotEmpty()
+
+				alias = resp.Value("alias").String().Raw()
+			}
+
+			// Redirect
+			testRedirect(t, alias, tc.url)
+
+			// Delete
+			e.DELETE("/"+path.Join("url", alias)).
+				WithBasicAuth("admin", "admin").
+				Expect().Status(http.StatusNoContent)
+
+			// Redirect again
+
+			testRedirectNotFound(t, alias)
+		})
+	}
+}
+
+func testRedirect(t *testing.T, alias string, urlToRedirect string) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+		Path:   alias,
+	}
+
+	redirectedToURL, err := api.GetRedirect(u.String())
+	require.NoError(t, err)
+
+	require.Equal(t, urlToRedirect, redirectedToURL)
+}
+
+func testRedirectNotFound(t *testing.T, alias string) {
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+		Path:   alias,
+	}
+
+	_, err := api.GetRedirect(u.String())
+	require.ErrorIs(t, err, api.ErrInvalidStatusCode)
+}
