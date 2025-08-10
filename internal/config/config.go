cea7c53d58471973eeb8d75374b1a11beed84089
Raccoon Slow <raccoons@student.21-school.ru>
2025-08-10 12:00:22 +0300

    fix: load passwords from .env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,12 @@
 package config
 
 import (
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
@@ -16,9 +18,9 @@ type Config struct {
 type Storage struct {
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
-	Database string `yaml:"database"`
+	User     string `yaml:"user" env:"POSTGRES_USER" env-required:"true"`
+	Password string `yaml:"password" env:"POSTGRES_PASSWORD" env-required:"true"`
+	Database string `yaml:"database" env:"POSTGRES_DB" env-required:"true"`
 	MaxConn  int32  `yaml:"max_conn" default:"10"`
 }
 
@@ -26,24 +28,28 @@ type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
-	User        string        `yaml:"user" env-required:"true"`
-	Password    string        `yaml:"password" env-required:"true"`
+	User        string        `yaml:"user" env:"HTTP_USER" env-required:"true"`
+	Password    string        `yaml:"password" env:"HTTP_PASSWORD" env-required:"true"`
 }
 
 func MustLoad() *Config {
+	// Загружаем .env
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: no .env file found: %v", err)
+	}
+
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
 
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config file does not exist: %s", configPath)
+		log.Fatalf("Config file does not exist: %s", configPath)
 	}
 
 	var cfg Config
-
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		log.Fatalf("cannot read config: %s", err)
+		log.Fatalf("Cannot read config: %s", err)
 	}
 
 	return &cfg
