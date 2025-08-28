6973a42f65c0d9a9110a954ef330b352f64bba22
Alina Sad <alina.sad@icloud.com>
2025-08-28 11:18:55 +0300

    fix: change .env

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -5,34 +5,41 @@ import (
 	"flag"
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/ilyakaznacheev/cleanenv"
-	"log"
-	"os"
-	"url-shortener/internal/config"
-
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
 	var migrationsPath, migrationsTable string
 
-	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations directory")
-	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "migrations table name")
+	flag.StringVar(&migrationsPath, "migrations-path", "./migrations", "Path to migrations folder")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "Table name")
 	flag.Parse()
 
-	if migrationsPath == "" {
-		panic("migrations path is required")
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Warning: no .env file found: %v", err)
 	}
 
-	cfg := MustLoad()
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbName := os.Getenv("POSTGRES_DB")
+
+	fmt.Println(dbUser, dbPassword, dbHost, dbPort, dbName, migrationsPath, migrationsTable)
 
 	// Формируем строку подключения
 	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&x-migrations-table=%s",
-		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database, migrationsTable)
+		dbUser, dbPassword, dbHost, dbPort, dbName, migrationsTable)
+	log.Printf("Connecting to database with URL: %s", dbURL)
 
+	// Выполняем миграции
 	m, err := migrate.New("file://"+migrationsPath, dbURL)
 	if err != nil {
+		log.Println("migrate.New")
 		panic(err)
 	}
 
@@ -45,23 +52,4 @@ func main() {
 	}
 
 	fmt.Println("migrations applied successfully")
-}
-
-func MustLoad() *config.Storage {
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		log.Fatal("CONFIG_PATH is not set")
-	}
-
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config file does not exist: %s", configPath)
-	}
-
-	var cfg config.Config
-
-	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		log.Fatalf("cannot read config: %s", err)
-	}
-
-	return &cfg.Storage
 }
